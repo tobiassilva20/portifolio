61ce96af59083653c175a51bbed34ddb9bbc5ee6
Tobias Silva <tobiassilva@hotmail.it>
2023-05-04 15:41:12 -0300

    Gitignore

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { Content } from '../content/content.component';
 
 @Component({
 	selector: 'app-body',
 	standalone: true,
-	imports: [NgbNavModule, NgbNavModule],
+	imports: [NgbNavModule, NgbNavModule, Content],
 	templateUrl: './body.html',
 	styleUrls: ['./style.css']
 })
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { Header } from './header/header';
-import { Body } from './body/body.component';
 import { NgbdNavVertical } from './nav-vertical/nav-vertical';
+import { Content } from './content/content.component';
+import { Body } from './body/body.component';
 
 @NgModule({
-  declarations: [
-    
-  ],
+  declarations: [],
   imports: [
-    Header, NgbdNavVertical, Body
+    Header, NgbdNavVertical, Body, Content, NgbModule
   ],
   providers: [],
-  exports: [Header, NgbdNavVertical, Body]
-
+  exports: [Header, NgbdNavVertical, Body, Content],
 })
 export class ComponentsModule { }
