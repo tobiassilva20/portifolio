c5ea8db3b6412daefe30f339d590066d8d02dc94
Tobias Silva <tobiassilva@hotmail.it>
2023-05-16 15:05:30 -0300

    Menu estilizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './components/content/about/about.component';
 import { EducationComponent } from './components/content/education/education.component';
 import { ProjectsComponent } from './components/content/projects/projects.component';
 import { SkillsComponent } from './components/content/skills/skills.component';
+
 const routes: Routes = [
-  { path: 'about', component: AboutComponent },
-  { path: 'education', component: EducationComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'skills', component: SkillsComponent },
+  { path: '', component: AboutComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'education', component: EducationComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'skills', component: SkillsComponent},
 ];
 
 @NgModule({
